c6ea83ec374b611877b9e2f91f328fbfb8b6eed3
Jeremias Erbs <jerry@badabam.de>
2022-03-16 10:53:13 +0100

    Add argTypes to EntryForm.stories

--- src/components/EntryForm.stories.js
+++ src/components/EntryForm.stories.js
@@ -3,6 +3,9 @@ import EntryForm from './EntryForm.js';
 export default {
   title: 'components/EntryForm',
   component: EntryForm,
+  argTypes: {
+    onSubmit: 'onSubmit',
+  },
 };
 
 const Template = args => <EntryForm {...args} />;
