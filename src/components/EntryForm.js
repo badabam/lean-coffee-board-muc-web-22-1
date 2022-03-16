a4d8b655a861fbd32883304ac9e220d09a09bcdd
Jeremias Erbs <jerry@badabam.de>
2022-03-16 10:51:25 +0100

    Create EntryForm and ScreenReaderOnly

---
+++ src/components/EntryForm.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import ScreenReaderOnly from './ScreenReaderOnly.js';
+
+export default function EntryForm({ onSubmit }) {
+  return (
+    <Form onSubmit={handleSubmit} aria-labelledby="entry-form-name">
+      <label htmlFor="text">
+        <ScreenReaderOnly>Entry text</ScreenReaderOnly>
+      </label>
+      <input
+        name="text"
+        id="text"
+        placeholder="Just some text ..."
+        type="text"
+      />
+      <PlusButton id="entry-form-name">
+        <ScreenReaderOnly>Create new entry</ScreenReaderOnly>
+        <div aria-hidden="true">+</div>
+      </PlusButton>
+    </Form>
+  );
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const inputElement = form.elements.text;
+    onSubmit(inputElement.value);
+  }
+}
+
+const Form = styled.form`
+  display: flex;
+  justify-content: space-between;
+
+  input {
+    width: 100%;
+    margin-right: 20px;
+  }
+`;
+
+const PlusButton = styled.button`
+  border-radius: 50%;
+  line-height: 0;
+  width: 28px;
+  height: 28px;
+  border: none;
+
+  div {
+    margin-top: -1px;
+  }
+`;
---
+++ src/components/EntryForm.stories.js
@@ -0,0 +1,11 @@
+import EntryForm from './EntryForm.js';
+
+export default {
+  title: 'components/EntryForm',
+  component: EntryForm,
+};
+
+const Template = args => <EntryForm {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
