209b445b19b86f9d9e426d44f895407511cdf151
Jeremias Erbs <jerry@badabam.de>
2022-03-16 15:14:24 +0100

    Add useSWR for fetching

--- src/App.js
+++ src/App.js
@@ -1,49 +1,54 @@
-import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import useSWR from 'swr';
 import Entry from './components/Entry';
 import EntryForm from './components/EntryForm';
 
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
 export default function App() {
-  const [entries, setEntries] = useState([]);
+  const {
+    data: entries,
+    error: entriesError,
+    mutate: mutateEntries,
+  } = useSWR('/api/entries', fetcher, {
+    refreshInterval: 1000,
+  });
 
-  useEffect(() => {
-    getEntries();
-
-    async function getEntries() {
-      const response = await fetch('/api/entries');
-      const entries = await response.json();
-      setEntries(entries);
-    }
-  }, []);
+  if (entriesError) return <h1>Sorry, could not fetch.</h1>;
 
   return (
     <Grid>
       <h1>Lean Coffee Board</h1>
       <EntryList role="list">
-        {entries.map(({ text, author }, index) => (
-          <li key={index}>
-            <Entry text={text} author={author} />
-          </li>
-        ))}
+        {entries
+          ? entries.map(({ text, author, _id }) => (
+              <li key={_id}>
+                <Entry text={text} author={author} />
+              </li>
+            ))
+          : '... loading ...'}
       </EntryList>
       <EntryForm onSubmit={handleNewEntry} />
     </Grid>
   );
 
   async function handleNewEntry(text) {
-    const response = await fetch('/api/entries', {
+    const newEntry = {
+      text,
+      author: 'Anonymous',
+    };
+
+    mutateEntries([...entries, newEntry], false);
+
+    await fetch('/api/entries', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        text,
-        author: 'Anonymous',
-      }),
+      body: JSON.stringify(newEntry),
     });
 
-    const newEntry = await response.json();
-    setEntries([...entries, newEntry]);
+    mutateEntries();
   }
 }
 
